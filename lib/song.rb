5e17b69c6c4ae8430efb27e4c55e1b65b7614778
Lorena Ramirez-Lopez <daleloreny@gmail.com>
2019-05-20 12:55:15 +0000

    Done.

--- lib/song.rb
+++ lib/song.rb
@@ -32,12 +32,15 @@ class Song
   
   
   def self.create_by_name(song_name) #class constructor
-  song = self.create #using the class constructor from before that creates a new instance when creating a new song. This adds it to the array
+    song = self.create 
+    song.name = song_name 
+    song 
+  end 
   
-  song.name = song_name #song.name is the part of the instance I want. song_name is my parameter 
-  
-  song #returns song
-  end
+  #INSTRUCTIONS FOR CLASS CONSTRUCTOR self.create_by_name
+  #using the class constructor from before that creates a new instance when creating a new song. This adds it to the array
+  #song.name is the part of the instance I want. song_name is my parameter 
+  #returns song
   
   def self.find_by_name(song_name) #class finder with parameter
     self.all.find {|song| song.name == song_name} 
@@ -61,12 +64,27 @@ class Song
 
   def self.new_from_filename(filename) #class constructor
     filename.split("-")
+    #WTF DO I DO HERE?!
+    #the filename must be parsed. separate the artist naem from the rest of the data based on the - delimiter. and when you parse the song name you have to remove the .mp3
     
-    #would I be able to just use the methods from above?
-    song = self.new #Song.create 
-    song.name = song_name #Song.new_by_name
+    song = self.new 
+    song.name = song_name 
     song.artist_name = artist_name 
     song
   end 
   
+  
+    def self.create_from_filename(filename) #class constructor
+    filename.split("-")
+    #WTF DO I DO HERE 
+   
+    song = self.create  
+    song.name = song_name 
+    song.artist_name = artist_name 
+    song
+  end 
+  
+  def self.destroy_all
+  self.all.delete 
+  end 
 end
