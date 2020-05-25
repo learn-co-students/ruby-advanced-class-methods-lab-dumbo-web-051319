0f9a39f4a15b826f37741a90e135c0ea24a6b829
Lorena Ramirez-Lopez <daleloreny@gmail.com>
2020-05-25 17:33:15 +0000

    Done.

--- lib/song.rb
+++ lib/song.rb
@@ -1,8 +1,7 @@
 class Song
   attr_accessor :name, :artist_name #this creates instances 
   
-  @@all = [] #class variable 
-             #Array to hold all instances 
+  @@all = [] #class variable / Array to hold all instances 
 
   def self.all #class method 
     @@all
@@ -13,23 +12,17 @@ class Song
   end
   
   def self.create #class constructor 
-  song = Song.new 
-  song.save 
-  song 
+    song = Song.new 
+    song.save 
+    song 
   end
 
   def self.new_by_name(song_name) #<- argument/parameters
-  song = self.new 
-  song.name = song_name
-  song 
+    song = self.new 
+    song.name = song_name
+    song 
   end 
-  #INSTRUCTIONS FOR CLASS CONSTRUCTOR
-  #song = self.new is constructing/creating a new song. 
-  #song.name is the part of the instance I want. 
-  #song_name is my parameter 
-  #returns song 
-  
-  
+
   
   def self.create_by_name(song_name) #class constructor
     song = self.create 
@@ -37,20 +30,11 @@ class Song
     song 
   end 
   
-  #INSTRUCTIONS FOR CLASS CONSTRUCTOR self.create_by_name
-  #using the class constructor from before that creates a new instance when creating a new song. This adds it to the array
-  #song.name is the part of the instance I want. song_name is my parameter 
-  #returns song
   
   def self.find_by_name(song_name) #class finder with parameter
     self.all.find {|song| song.name == song_name} 
   end 
-    #INSTRUCTIONS FOR CLASS FINDER 
-    #self.all goes through my entire list/array
-    #find is the method to find the parameters 
-    #|song| is the singular item of my list that Im look for
-    #song.name == song_name is the specificaions/condition for the singular item parameters
-  
+
   
   def self.find_or_create_by_name(song_name) #class method with parameter
     self.find_by_name(song_name) || self.create_by_name(song_name)
@@ -62,29 +46,32 @@ class Song
     self.all.sort_by{|song| song.name}
   end
 
-  def self.new_from_filename(filename) #class constructor
-    filename.split("-")
-    #WTF DO I DO HERE?!
-    #the filename must be parsed. separate the artist naem from the rest of the data based on the - delimiter. and when you parse the song name you have to remove the .mp3
-    
-    song = self.new 
-    song.name = song_name 
-    song.artist_name = artist_name 
-    song
-  end 
-  
-  
-    def self.create_from_filename(filename) #class constructor
-    filename.split("-")
-    #WTF DO I DO HERE 
+  def self.new_from_filename(filename)
+   split = filename.chomp(".mp3").split(" - ")
+   artist = split[0]
+   title = split[1]
    
-    song = self.create  
-    song.name = song_name 
-    song.artist_name = artist_name 
-    song
+   song = self.new
+   song.name = title
+   song.artist_name = artist 
+   song
+  end
+  
+  
+  def self.create_from_filename(filename) 
+   split = filename.chomp(".mp3").split(" - ")
+   artist = split[0]
+   title = split[1]
+   
+   song = self.create 
+   song.name = title 
+   song.artist_name = artist 
+   song
   end 
   
   def self.destroy_all
-  self.all.clear
+    self.all.clear
   end 
 end
+
+  
